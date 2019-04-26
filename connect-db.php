15ab1f39f80a6a94e43546b6b0e67e8274f92582
kfang2 <47185626+kfang2@users.noreply.github.com>
2019-04-26 00:09:49 -0400

    commit

--- connect-db.php
+++ connect-db.php
@@ -1,22 +1,15 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'queens');
-define('DB_PASSWORD', 'coffee');
-define('DB_NAME', 'queens');
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+// Database Variables (edit with your own server information)
+$server = 'localhost';
+$user = 'queens';
+$pass = 'coffee';
+$db = 'queens';
+// Connect to Database
+$connection = mysqli_connect($server,$user,$pass,$db);
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 ?>
-<!--  
-// // Connect to Database
-// $connection = mysqli_connect($server,$user,$pass,$db);
-// if (!$connection) {
-//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
-// ?> -->
